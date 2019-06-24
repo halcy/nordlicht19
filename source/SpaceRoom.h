9cae2e680eea5eba1ff852025a82a87045957f2b
Lorenz Diener <lorenzd@gmail.com>
2019-06-25 01:29:45 +0200

    Missing stuff

---
+++ source/SpaceRoom.h
@@ -0,0 +1,16 @@
+#ifndef SPACEROOM_H
+#define SPACEROOM_H
+
+#include "Rasterize.h"
+
+#define numVerticesSpaceRoom 158
+#define numNormalsSpaceRoom 140
+#define numFacesSpaceRoom 284
+
+extern const init_vertex_t verticesSpaceRoom[];
+extern const init_vertex_t normalsSpaceRoom[];
+extern const index_triangle_t facesSpaceRoom[];
+extern const vec2_t texcoordsSpaceRoom[];
+
+#endif
+
